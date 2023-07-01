dacd52da3b96cde071885ac1b63fd6f2f93b4d75
mecies <maciekhnat@gmail.com>
2023-07-01 16:27:48 +0200

    Add proper component implementation, stories, and tests

--- src/components/button/index.stories.tsx
+++ src/components/button/index.stories.tsx
@@ -1,11 +1,59 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { Plus, Forward } from "lucide-react";
 
 import Button from ".";
-import { PRIMARY_BUTTON_TEXT, SECONDARY_BUTTON_TEXT } from "./mock-data";
+import {
+  OVERFLOWING_TEXT,
+  PRIMARY_BUTTON_TEXT,
+  SECONDARY_BUTTON_TEXT,
+} from "./mock-data";
+
+const ICONS = {
+  Plus,
+  Forward,
+  None: undefined,
+};
 
 const meta: Meta<typeof Button> = {
   title: "Button",
   component: Button,
+  argTypes: {
+    children: {
+      control: "text",
+      description: "The button's text.",
+    },
+    variant: {
+      control: "radio",
+      options: ["PRIMARY", "SECONDARY"],
+      description: "Controls the button's appearance.",
+    },
+    size: {
+      control: "radio",
+      options: ["LARGE", "SMALL"],
+      description: "Controls the button's size.",
+    },
+    fillContainer: {
+      control: "boolean",
+      description: "Controls whether the button fills its container.",
+    },
+    onClick: {
+      action: "clicked",
+      description: "Fired when the button is clicked.",
+    },
+    icon: {
+      options: Object.keys(ICONS),
+      mapping: ICONS,
+      control: {
+        type: "select",
+      },
+      description: "Controls the button's icon.",
+    },
+    iconPosition: {
+      control: "radio",
+      options: ["LEFT", "RIGHT"],
+      description: "Controls the button's icon position.",
+    },
+  },
   tags: ["autodocs"],
 };
 
@@ -16,11 +64,65 @@ type Story = StoryObj<typeof Button>;
 export const Primary: Story = {
   args: {
     children: PRIMARY_BUTTON_TEXT,
+    size: "LARGE",
+    variant: "PRIMARY",
+    fillContainer: false,
+    icon: undefined,
   },
 };
 
 export const Secondary: Story = {
   args: {
+    ...Primary.args,
     children: SECONDARY_BUTTON_TEXT,
+    variant: "SECONDARY",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    ...Primary.args,
+    size: "SMALL",
+  },
+};
+
+export const FillContainer: Story = {
+  args: {
+    ...Primary.args,
+    fillContainer: true,
+  },
+};
+
+export const WithIconRight: Story = {
+  args: {
+    ...Primary.args,
+    icon: ICONS.Plus,
+  },
+};
+
+export const WithIconLeft: Story = {
+  args: {
+    ...Primary.args,
+    icon: ICONS.Plus,
+    iconPosition: "LEFT",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Primary.args,
+    disabled: true,
+  },
+};
+
+export const Truncated: Story = {
+  args: {
+    ...Primary.args,
+    children: OVERFLOWING_TEXT,
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
   },
 };
---
+++ src/components/button/style.ts
@@ -0,0 +1,133 @@
+import { css, styled } from "styled-components";
+import { darken } from "polished";
+import { LucideProps } from "lucide-react";
+
+export type Variant = "PRIMARY" | "SECONDARY";
+export type Size = "SMALL" | "LARGE";
+
+type TextWrapperProps = {
+  $hasIcon: boolean;
+};
+
+type WrapperProps = {
+  $variant: Variant;
+  $size: Size;
+  $fillContainer: boolean;
+};
+
+export const ICON_SIZE = 20;
+export const GAP_SIZE = 16;
+
+export const VARIANT_TO_ICON_PROPS_MAP: Record<Variant, LucideProps> = {
+  PRIMARY: {
+    color: "#fff",
+  },
+  SECONDARY: {
+    color: "#1f99d6",
+  },
+};
+
+function getStyleForVariant(variant: Variant) {
+  if (variant === "PRIMARY") {
+    return css`
+      border-radius: 50px;
+      border: 1px solid #cd141a;
+      background: #cd141a;
+      color: #fff;
+
+      &:hover {
+        border: 1px solid ${darken(0.05, "#cd141a")};
+        background: ${darken(0.05, "#cd141a")};
+      }
+
+      &:active {
+        border: 1px solid ${darken(0.1, "#cd141a")};
+        background: ${darken(0.1, "#cd141a")};
+      }
+    `;
+  }
+
+  return css`
+    border-radius: 50px;
+    border: 1px solid #1f99d6;
+    background: #fff;
+    color: #1f99d6;
+
+    &:hover {
+      background: ${darken(0.05, "#fff")};
+    }
+
+    &:active {
+      background: ${darken(0.1, "#fff")};
+    }
+  `;
+}
+
+function getStyleForSize(size: Size) {
+  if (size === "SMALL") {
+    return css`
+      font-size: 16px;
+      padding: 10px 20px;
+    `;
+  }
+
+  return css`
+    font-size: 21px;
+    padding: 16px 30px;
+  `;
+}
+
+function getStyleForFillContainer(fillContainer: boolean) {
+  if (fillContainer) {
+    return css`
+      width: 100%;
+    `;
+  }
+}
+
+function getStyleForDisabled(disabled: boolean) {
+  if (disabled) {
+    return css`
+      cursor: not-allowed;
+    `;
+  }
+
+  return css`
+    cursor: pointer;
+  `;
+}
+
+export const Wrapper = styled.button<WrapperProps>`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  gap: ${GAP_SIZE}px;
+  max-width: 100%;
+
+  font-family: Nunito Sans;
+  font-style: normal;
+  font-weight: 700;
+
+  transition: background-color 0.1s ease, border-color 0.1s ease;
+
+  &:focus-visible {
+    outline: 2px solid #2474b6;
+    outline-offset: 2px;
+  }
+
+  ${({ disabled }) => getStyleForDisabled(!!disabled)};
+  ${({ $variant }) => getStyleForVariant($variant)};
+  ${({ $size }) => getStyleForSize($size)};
+  ${({ $fillContainer }) => getStyleForFillContainer($fillContainer)};
+`;
+
+export const TextWrapper = styled.span<TextWrapperProps>`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  max-width: calc(
+    100% - ${({ $hasIcon }) => ($hasIcon ? ICON_SIZE + GAP_SIZE : 0)}px
+  );
+`;
