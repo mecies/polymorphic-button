06d617ab93f9132eaf530495e0dd8717472d5a31
mecies <maciekhnat@gmail.com>
2023-07-01 16:28:13 +0200

    Add missing libraries and enhance storybook setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,25 @@
-import type { StorybookConfig } from '@storybook/nextjs';
+import * as path from "path";
+import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-a11y",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
     {
-      name: '@storybook/addon-styling',
+      name: "@storybook/addon-styling",
       options: {},
     },
   ],
   framework: {
-    name: '@storybook/nextjs',
-    options: {},
+    name: "@storybook/nextjs",
+    options: {
+      nextConfigPath: path.resolve(__dirname, "../next.config.js"),
+    },
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
 };
 export default config;
